d6829ed2937cb521800b3deb2c894badfc440c22
briana-mcn <briana.mcn@gmail.com>
2019-12-06 23:31:14 -0600

    Configure mysql local database; Install dependencies

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 
 
 class Config:
-    pass
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql://dev:test@localhost:3306/road_to_nowhere'
 
 
 class ConfigDev(Config):
-    pass
-
-
-class ConfigTest(Config):
-    pass
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql://dev:test@localhost:3306/road_to_nowhere'
 
 
 class ConfigProd(Config):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql://dev:test@localhost:3306/road_to_nowhere'
     pass

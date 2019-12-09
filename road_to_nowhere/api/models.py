935ed1c7294da29196854714e8284e57b48087b1
briana-mcn <briana.mcn@gmail.com>
2019-12-08 21:59:09 -0600

    Add repr methods to models; add verses column to Song

--- road_to_nowhere/api/models.py
+++ road_to_nowhere/api/models.py
@@ -4,21 +4,28 @@ from road_to_nowhere import db
 # nullable set to False means 'not null' added to the DDL
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     albums = db.relationship('Album', back_populates='artist')
 
+    def __repr__(self):
+        return f'<Artist {self.name}>'
 
 class Album(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
     artist = db.relationship('Artist', back_populates='albums')
     songs = db.relationship('Song', back_populates='album')
 
+    def __repr__(self):
+        return f'<Album {self.name, self.artist_id}>'
 
 class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    album_id = db.Column(db.Integer, db.ForeignKey('album.id'))
+    name = db.Column(db.String(300), nullable=False)
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=False)
     album = db.relationship('Album', back_populates='songs')
+    verses = db.Colum(db.Text, nullable=False)
 
+    def __repr__(self):
+        return f'<Song {self.name}, {self.album_id}>'
